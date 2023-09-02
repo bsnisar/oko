1410acfee325314b35d66cd06ee453d7bc10ebf4
Bohdan Snisar <bohdan.snisar@revolut.com>
2023-09-02 12:28:54 +0200

    next changes

---
+++ README.md
@@ -0,0 +1,7 @@
+## Install 
+
+```
+$ source /path/to/venv/bin/activate
+(env) $ python -m pip install pip-tools
+(env) $ pip-compile
+```
--- pix-weaviate/app.py
+++ pix-weaviate/app.py
@@ -1,10 +1,31 @@
 import weaviate
 
+import os
+from logging import getLogger
+from fastapi import FastAPI, Response, status
+from contextlib import asynccontextmanager
 
-client = weaviate.Client("https://some-endpoint.weaviate.network") 
+logger = getLogger('uvicorn')
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+	global client
+
+    # env = os.getenv("WEAVIATE_ENV", default="http://127.0.0.1")
+    # env = "http://127.0.0.1"
+    logger.info("Running weaviate %s", env)
+	client = weaviate.Client("http://127.0.0.1") 
+
+	logger.info("Initialization complete")
+	yield
 
 
+app = FastAPI(lifespan=lifespan)
 
-client.schema.get()  # Get the schema to test connection
+
+@app.get("/.well-known/live", response_class=Response)
+@app.get("/.well-known/ready", response_class=Response)
+async def live_and_ready(response: Response):
+	response.status_code = status.HTTP_204_NO_CONTENT
 
 
